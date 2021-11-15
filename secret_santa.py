bcb8f89e2aaf11b343ed077d3e8b59e367252383
Eddie Barun <52569294+eddiebarun@users.noreply.github.com>
2021-11-15 22:15:34 +0000

    Add files via upload
    v1

---
+++ secret_santa.py
@@ -0,0 +1,15 @@
+import random
+
+members = ["foo", "bar", "test"]
+members_temp = members.copy()
+
+random.shuffle(members_temp)
+finish =  False
+x = 0
+
+for x in range(len(members_temp)):
+    if members[x] == str(members_temp[x]):
+        members_temp[x], members_temp[x+1] = members_temp[x+1], members_temp[x]
+    
+for x in range(len(members)):
+    print("%s gets %s" % (members[x], str(members_temp[x])))
